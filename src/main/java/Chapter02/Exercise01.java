90ecc8e6f9787d7353bbb5118984c11fa00b911f
Stephen Hodgson <stephen@srhodgson.co.uk>
2025-12-14 22:47:56 +0000

    Chapter02 Exercises 01, 02, and 03
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ src/main/java/Chapter02/Exercise01.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        String dinoBreed = "T-Rex";
+        float dinoHeight = 25.4f;
+        float dinoLength = 15.5f;
+        float dinoWeight = 1800f;
+
+        System.out.println("This a " + dinoBreed + ", it is " + dinoHeight + "m tall, " + dinoLength + "m long, and weighs" + dinoWeight + "lbs.");
+    }
+}
---
+++ src/main/java/Chapter02/Exercise02.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        String dinoName = "Bob";
+        int dinoAge = 37;
+        boolean isCarnivore = true;
+
+        System.out.println("The dino's name is " + dinoName + ", it is " + dinoAge + " years old. Is it a carnivore? "
+                + isCarnivore);
+    }
+}

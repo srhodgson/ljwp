9d8b06451becb5cfbcb9919a43ae86d50300174d
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-02-21 22:51:41 +0000

    Exercise 1

---
+++ src/main/java/Chapter05/Exercise01.java
@@ -0,0 +1,9 @@
+package Chapter05;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("dino"+i);
+        }
+    }
+}

5936ab1e0596e37e54f49f1d8624563869ab6c42
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-02-21 22:52:11 +0000

    Remove public

--- src/main/java/Chapter05/Exercise01.java
+++ src/main/java/Chapter05/Exercise01.java
@@ -1,7 +1,7 @@
 package Chapter05;
 
 public class Exercise01 {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.println("dino"+i);
         }

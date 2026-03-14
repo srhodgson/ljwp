6fc77a50880581c935ae15f2b790bce7fca0ebf1
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-14 23:21:30 +0000

    Start the project

---
+++ src/main/java/Chapter06/DinoTracker.java
@@ -0,0 +1,15 @@
+package Chapter06;
+
+public class DinoTracker {
+    static void main(String[] args) {
+        // Create for arrays each with a size of 10 called dinoNames, dinoAges, dinoSpecies, dinoEnclosures
+
+        // Enter the details for 10 dinos in each array
+
+        // Write a loop that goes through the arrays and prints the details in a readable format
+
+        // print the average age and weight of each dino.
+        // You will need to sum up all ages and weights in the respective arrays and then divide by the number of dinos
+
+    }
+}
